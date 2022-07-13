615a5727fea937bdea25e7567bd9e2a78e8810e4
CaioDev <53819717+CaioGitDev@users.noreply.github.com>
2022-07-13 20:57:28 +0100

    feat: add transaction styles

--- src/components/NewTransactionModal/NewTransactionModal.tsx
+++ src/components/NewTransactionModal/NewTransactionModal.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactModal from 'react-modal';
 
 import { Container } from './NewTransactionModal.styles';
+import CloseImage from '../../assets/close.svg';
 
 interface INewTransactionModalProps {
   isOpen: boolean;
@@ -16,7 +17,22 @@ export const NewTransactionModal: React.FC<INewTransactionModalProps> = ({
     <ReactModal
       isOpen={isOpen}
       onRequestClose={OnRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
     >
+      <button type="button" className='react-modal-close' onClick={OnRequestClose}>
+        <img src={CloseImage} alt="Fechar" />
+      </button>
+      <Container>
+        <h2>Registrar Transação</h2>
+        <input type="text" placeholder="Título" />
+        <input type="number" placeholder="Valor" />
+        <input type="text" placeholder="Categoria" />
+
+        <button type="submit">
+          Registrar
+        </button>
+      </Container>
 
     </ReactModal>
   );
