4cadd4af7bb7286b46f446f080135919197736b6
CaioDev <53819717+CaioGitDev@users.noreply.github.com>
2022-07-15 00:30:32 +0100

    feat: create summary

--- src/components/Summary/Summary.tsx
+++ src/components/Summary/Summary.tsx
@@ -10,6 +10,23 @@ export const Summary: React.FC = () => {
 
   const { transactions } = useContext(TransactionContext);
 
+  const summary = transactions.reduce((acc, transaction) => {
+
+    if (transaction.type === 'deposit') {
+      acc.deposits += transaction.value;
+      acc.total += transaction.value;
+    } else if (transaction.type === 'withdraw') {
+      acc.withdrawals += transaction.value;
+      acc.total -= transaction.value;
+    }
+
+    return acc;
+  }, {
+    deposits: 0,
+    withdrawals: 0,
+    total: 0
+  })
+
   return (
 
     <Container>
@@ -18,21 +35,31 @@ export const Summary: React.FC = () => {
           <p>Entradas</p>
           <img src={incomeIcon} alt="Entradas" />
         </header>
-        <strong> + €1000</strong>
+        <strong>{new Intl.NumberFormat('pt-PT', {
+          style: 'currency',
+          currency: 'EUR'
+        }).format(summary.deposits)}
+        </strong>
       </div>
       <div>
         <header>
           <p>saídas</p>
           <img src={outcomeIcon} alt="Saídas" />
         </header>
-        <strong> - €195</strong>
+        <strong>- {new Intl.NumberFormat('pt-PT', {
+          style: 'currency',
+          currency: 'EUR'
+        }).format(summary.withdrawals)}</strong>
       </div>
       <div>
         <header>
           <p>Total</p>
           <img src={totalIcon} alt="Total" />
         </header>
-        <strong>€3552</strong>
+        <strong>{new Intl.NumberFormat('pt-PT', {
+          style: 'currency',
+          currency: 'EUR'
+        }).format(summary.total)}</strong>
       </div>
     </Container>
   );
